6ce8b535094b5a8f2f58e48c9213b73c0c4df483
ZLF1833547977 <91466305+ZLF1833547977@users.noreply.github.com>
2025-03-25 14:14:39 +0800

    Update basic.vue

--- docs/examples/button/basic.vue
+++ docs/examples/button/basic.vue
@@ -1,48 +1,61 @@
 <template>
-  <div class="mb-4">
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-  </div>
-
-  <div class="mb-4">
-    <el-button plain>Plain</el-button>
-    <el-button type="primary" plain>Primary</el-button>
-    <el-button type="success" plain>Success</el-button>
-    <el-button type="info" plain>Info</el-button>
-    <el-button type="warning" plain>Warning</el-button>
-    <el-button type="danger" plain>Danger</el-button>
-  </div>
-
-  <div class="mb-4">
-    <el-button round>Round</el-button>
-    <el-button type="primary" round>Primary</el-button>
-    <el-button type="success" round>Success</el-button>
-    <el-button type="info" round>Info</el-button>
-    <el-button type="warning" round>Warning</el-button>
-    <el-button type="danger" round>Danger</el-button>
-  </div>
-
   <div>
-    <el-button :icon="Search" circle />
-    <el-button type="primary" :icon="Edit" circle />
-    <el-button type="success" :icon="Check" circle />
-    <el-button type="info" :icon="Message" circle />
-    <el-button type="warning" :icon="Star" circle />
-    <el-button type="danger" :icon="Delete" circle />
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="name" label="姓名">
+        <template #default="{ row, $index }">
+          <el-form-item :prop="`tableData[${$index}].name`" :rules="rules.name">
+            <el-input v-model="row.name" @blur="validateCell(row, 'name')"></el-input>
+          </el-form-item>
+        </template>
+      </el-table-column>
+      <el-table-column prop="age" label="年龄">
+        <template #default="{ row, $index }">
+          <el-form-item :prop="`tableData[${$index}].age`" :rules="rules.age">
+            <el-input v-model="row.age" @blur="validateCell(row, 'age')"></el-input>
+          </el-form-item>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
-<script lang="ts" setup>
-import {
-  Check,
-  Delete,
-  Edit,
-  Message,
-  Search,
-  Star,
-} from '@element-plus/icons-vue'
+<script setup>
+import { ref } from 'vue';
+import { ElTable, ElTableColumn, ElInput, ElFormItem } from 'element-plus';
+
+const tableData = ref([
+  { name: '', age: '' },
+  { name: '', age: '' }
+]);
+
+const rules = ref({
+  name: [
+    { required: true, message: '姓名不能为空', trigger: 'blur' }
+  ],
+  age: [
+    { required: true, message: '年龄不能为空', trigger: 'blur' },
+    { type: 'number', message: '年龄必须为数字', trigger: 'blur' }
+  ]
+});
+
+const validateCell = (row, prop) => {
+  const formItem = document.querySelector(`[prop="tableData[${tableData.value.indexOf(row)}].${prop}"]`);
+  if (formItem) {
+    const form = formItem.closest('.el-form');
+    if (form) {
+      const formInstance = form.__vueParentComponent;
+      if (formInstance) {
+        formInstance.validateField(`tableData[${tableData.value.indexOf(row)}].${prop}`, (errors) => {
+          if (errors) {
+            console.log(errors);
+          }
+        });
+      }
+    }
+  }
+};
 </script>
+
+<style scoped>
+/* 可根据需要添加样式 */
+</style>   
